678ca7328bbb4563634e42a6e5b357ba95f3c7e0
nrani039 <73906799+nrani039@users.noreply.github.com>
2023-11-17 18:50:51 -0700

    Update searchbrowse.js

--- js/searchbrowse.js
+++ js/searchbrowse.js
@@ -1,5 +1,11 @@
 // search browse songs 
 
+// Milestone 2 (due Nov 17). Implement part of the Search/Browse Songs view. You must
+// have the artist and genre select lists displaying the data in the provided json files. As well,
+// display the song list (title, artist name, year, genre name, popularity) using the provided
+// sample-songs.json data. Implement the column sort as well. To submit your milestone,
+// simply email me with the URL of the home page of the site on github pages.
+
 //artist dropdown
 document.write("<h2>Selecting Artists</h2>");
 const artists = JSON.parse(content);
@@ -31,6 +37,7 @@ function outputArtist(genre) {
 //songs
 document.write("<h2>Browse/Search result</h2>");
 const songList = JSON.parse(songsFile);
+console.log(songList);
 
   function outputSongs(song){
     document.write(`<option value="${song.song_id}">${song.title}</option>`);
@@ -63,6 +70,49 @@ document.write(`<p id="songSelect">`);
     console.log(songviews);
   }
   document.write(`</p>`);
+
+ 
+
+
+  // Get the container to append the table
+const tableContainer = document.getElementById("table-container");
+
+// Create a table element
+const table = document.createElement("table");
+
+// Create table header
+const headerRow = table.insertRow(0);
+const titleHeader = headerRow.insertCell(0);
+titleHeader.innerHTML = "<b>Title</b>";
+const artistHeader = headerRow.insertCell(1);
+artistHeader.innerHTML = "<b>Artist</b>";
+const yearHeader = headerRow.insertCell(2);
+yearHeader.innerHTML = "<b>Year</b>";
+const popHeader = headerRow.insertCell(3);
+popHeader.innerHTML = "<b>Popularity</b>";
+const genreHeader = headerRow.insertCell(4);
+genreHeader.innerHTML = "<b>Genre</b>";
+
+
+// Iterate through each song and add a row to the table
+for (let i = 0; i < songList.length; i++) {
+    const song = songList[i];
+    const row = table.insertRow(i + 1);
+    const titleCell = row.insertCell(0);
+    titleCell.innerHTML = song.title;
+    const artistCell = row.insertCell(1);
+    artistCell.innerHTML = song.artist.name;
+    const yearCell = row.insertCell(2);
+    yearCell.innerHTML = song.year;
+    const popularity = row.insertCell(3);
+    popularity.innerHTML = song.details.popularity;
+    const genre = row.insertCell(4);
+    genre.innerHTML = song.genre.name;
+
+}
+
+// Append the table to the container
+tableContainer.appendChild(table);
 
 
  // const songList = JSON.parse(songsFile);
