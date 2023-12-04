5eb1df80b54606b10743061691a371748f5cf71e
marselyai <marselyaf@gmail.com>
2023-12-03 20:22:36 -0700

    hover effect over rows

--- js/playlistView.js
+++ js/playlistView.js
@@ -1,1 +1,36 @@
 //play list view page 
+document.addEventListener("DOMContentLoaded", function () {
+    const selectedSongs = JSON.parse(localStorage.getItem('selectedSongs')) || [];
+    displaySelectedSongs(selectedSongs);
+});
+
+function displaySelectedSongs(songs) {
+    const playlistContainer = document.getElementById("playlist-container");
+
+    const table = document.createElement("table");
+    const headerRow = table.insertRow();
+    const headers = ["Title", "Artist", "Year", "Popularity", "Genre"];
+    
+    // Create table headers
+    headers.forEach(headerText => {
+        const headerCell = document.createElement("th");
+        headerCell.textContent = headerText;
+        headerRow.appendChild(headerCell);
+    });
+
+    // Create table rows for selected songs
+    songs.forEach(song => {
+        const row = table.insertRow();
+        const columns = ["title", "artist", "year", "popularity", "genre"];
+
+        columns.forEach(column => {
+            const cell = row.insertCell();
+            cell.textContent = song[column];
+        });
+    });
+
+    playlistContainer.appendChild(table);
+}
+
+
+
--- js/searchbrowse.js
+++ js/searchbrowse.js
@@ -1,10 +1,9 @@
 
 //recent version 
-
-document.addEventListener("DOMContentLoaded", function () {
-
-    const tableContainer = this.getElementById("table-container");
-    const dropdown = this.getElementById("dropdown");
+  //trial 
+  document.addEventListener("DOMContentLoaded", function () {
+    const tableContainer = document.getElementById("table-container");
+    const dropdown = document.getElementById("dropdown");
 
     const selectArtists = document.createElement('select');
     const selectGenres = document.createElement('select');
@@ -18,130 +17,165 @@ document.addEventListener("DOMContentLoaded", function () {
     const addButton = document.createElement('button');
     addButton.textContent = 'Add';
     addButton.addEventListener('click', function () {
-        window.location.href = 'js/playListView.js'; 
+    
+        const selectedSongs = getSelectedSongs();
+        localStorage.setItem('selectedSongs', JSON.stringify(selectedSongs));
+        window.location.href = 'js/playlistView.js';
     });
     dropdown.appendChild(addButton);
-  
+
+
+    function toggleRowSelection(row) {
+      row.classList.toggle("selected");
+  }
+
+  tableContainer.addEventListener("click", function (event) {
+      const clickedElement = event.target;
+
+      if (clickedElement.tagName === "TD" && clickedElement.parentNode.tagName === "TR") {
+          toggleRowSelection(clickedElement.parentNode);
+      }
+  });
+
     const url = 'https://www.randyconnolly.com/funwebdev/3rd/api/music/songs-nested.php';
-  
+
     let songs = []; // Declare the variable outside the function
     let sortColumn = null;
     let sortDirection = 1;
-  
+
     fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        // Store data in localStorage for future use
-        localStorage.setItem('songData', JSON.stringify(data));
-  
-        // Assign the data to the songs variable
-        songs = Array.isArray(data) ? data : [];
-  
-        // Extract unique artists
-        const artists = [...new Set(songs.map(song => song.artist.name))];
-        // Extract unique genres
-        const genres = [...new Set(songs.map(song => song.genre.name))];
-  
-        // Populate select dropdown for artists
-        artists.forEach(artist => {
-          const optionArtist = document.createElement('option');
-          optionArtist.textContent = artist;
-          selectArtists.appendChild(optionArtist);
+        .then(response => response.json())
+        .then(data => {
+            // Store data in localStorage for future use
+            localStorage.setItem('songData', JSON.stringify(data));
+
+            // Assign the data to the songs variable
+            songs = Array.isArray(data) ? data : [];
+
+            // Extract unique artists
+            const artists = [...new Set(songs.map(song => song.artist.name))];
+            // Extract unique genres
+            const genres = [...new Set(songs.map(song => song.genre.name))];
+
+            // Populate select dropdown for artists
+            artists.forEach(artist => {
+                const optionArtist = document.createElement('option');
+                optionArtist.textContent = artist;
+                selectArtists.appendChild(optionArtist);
+            });
+
+            // Populate select dropdown for genres
+            genres.forEach(genre => {
+                const optionGenre = document.createElement('option');
+                optionGenre.textContent = genre;
+                selectGenres.appendChild(optionGenre);
+            });
+
+            // Create and sort the table
+            makeTable(songs);
+
+            headers.forEach((header, index) => {
+                const headerCell = headerRow.cells[index];
+                headerCell.addEventListener('click', function () {
+                    sortTable(index);
+                });
+            });
+
+            loader.style.display = 'none';
+            tableContainer.style.display = 'block';
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+            loader.textContent = 'Error fetching data';
         });
-  
-        // Populate select dropdown for genres
-        genres.forEach(genre => {
-          const optionGenre = document.createElement('option');
-          optionGenre.textContent = genre;
-          selectGenres.appendChild(optionGenre);
-        });
-  
-        // Create and sort the table
-        makeTable(songs);
-  
-        headers.forEach((header, index) => {
-          const headerCell = headerRow.cells[index];
-          headerCell.addEventListener('click', function () {
-            sortTable(index);
-          });
-        });
-  
-        loader.style.display = 'none';
-        tableContainer.style.display = 'block';
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-        loader.textContent = 'Error fetching data';
-      });
-  
+
     const headers = ["Title ^", "Artist ^", "Year ^", "Popularity ^", "Genre ^"];
     const table = document.createElement("table");
     const headerRow = table.insertRow(0);
-  
+
     function createHeaderCell(headerText, columnIndex) {
-      const header = document.createElement("th");
-      header.innerHTML = "<b>" + headerText + "</b>";
-      header.addEventListener("click", function() {
-        sortTable(columnIndex);
-      });
-      return header;
+        const header = document.createElement("th");
+        header.innerHTML = "<b>" + headerText + "</b>";
+        header.addEventListener("click", function () {
+            sortTable(columnIndex);
+        });
+        return header;
     }
-  
+
     for (const headerText of headers) {
-      headerRow.appendChild(createHeaderCell(headerText, headers.indexOf(headerText)));
-    }
-  
-    function getColumnValue(song, columnIndex) {
-      const column = ["title", "artist.name", "year", "details.popularity", "genre.name"];
-      let value = song;
-      for (const prop of column[columnIndex].split('.')) {
-        value = value && value[prop];
-      }
-      return value;
-    }
-  
-    function makeTable(songs) {
-      table.innerHTML = "";
-      const headerRow = table.insertRow(0);
-      for (const headerText of headers) {
         headerRow.appendChild(createHeaderCell(headerText, headers.indexOf(headerText)));
-      }
-  
-      for (const song of songs) {
-        const row = table.insertRow();
-        for (const headerText of headers) {
-          const cell = row.insertCell();
-          cell.innerHTML = getColumnValue(song, headers.indexOf(headerText));
+    }
+
+    function getColumnValue(song, columnIndex) {
+        const column = ["title", "artist.name", "year", "details.popularity", "genre.name"];
+        let value = song;
+        for (const prop of column[columnIndex].split('.')) {
+            value = value && value[prop];
         }
-      }
-  
-      tableContainer.innerHTML = "";
-      tableContainer.appendChild(table);
+        return value;
     }
-  
+
+    function makeTable(songs) {
+        table.innerHTML = "";
+        const headerRow = table.insertRow(0);
+        for (const headerText of headers) {
+            headerRow.appendChild(createHeaderCell(headerText, headers.indexOf(headerText)));
+        }
+
+        for (const song of songs) {
+            const row = table.insertRow();
+            for (const headerText of headers) {
+                const cell = row.insertCell();
+                cell.innerHTML = getColumnValue(song, headers.indexOf(headerText));
+                cell.dataset.songId = song.song_id; // Set the dataset for the song ID
+                cell.addEventListener('click', function () {
+                    toggleSelected(cell);
+                });
+            }
+        }
+
+        tableContainer.innerHTML = "";
+        tableContainer.appendChild(table);
+    }
+
     function sortTable(columnIndex) {
-      if (sortColumn === null) {
-        sortColumn = columnIndex;
-      } else if (sortColumn === columnIndex) {
-        sortDirection *= -1;
-      } else {
-        sortDirection = 1;
-        sortColumn = columnIndex;
-      }
-  
-      songs.sort(function(a, b) {
-        const columnA = getColumnValue(a, columnIndex);
-        const columnB = getColumnValue(b, columnIndex);
-        return columnA < columnB ? -sortDirection : (columnA > columnB ? sortDirection : 0);
-      });
-  
-      makeTable(songs);
+        if (sortColumn === null) {
+            sortColumn = columnIndex;
+        } else if (sortColumn === columnIndex) {
+            sortDirection *= -1;
+        } else {
+            sortDirection = 1;
+            sortColumn = columnIndex;
+        }
+
+        songs.sort(function (a, b) {
+            const columnA = getColumnValue(a, columnIndex);
+            const columnB = getColumnValue(b, columnIndex);
+            return columnA < columnB ? -sortDirection : (columnA > columnB ? sortDirection : 0);
+        });
+
+        makeTable(songs);
     }
-  });
+
+    function toggleSelected(cell) {
+        if (cell.classList.contains('selected')) {
+            cell.classList.remove('selected');
+        } else {
+            cell.classList.add('selected');
+        }
+    }
+
+    function getSelectedSongs() {
+        const selectedCells = document.querySelectorAll('.selected');
+        const selectedSongIds = Array.from(selectedCells).map(cell => cell.dataset.songId);
+        return selectedSongIds;
+    }
+});
+
+
+
+
   
-
-
-
 
 
      
